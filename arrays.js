5efc31da2db2457198e5bd8fc54e52e70a54a04c
Quinten Krauth <quintenkrauth19@gmail.com>
2022-09-16 10:39:25 -0400

    assessment save

--- arrays.js
+++ arrays.js
@@ -3,12 +3,12 @@
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+let colorCopy = faveColors.slice(0,3)
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
-//CODE HERE
+colorCopy.push("blue")
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -18,7 +18,7 @@ const faveColors = ['red', 'green', 'black']
 
 const numbers = [1, 2, 3, 4, 5]
 
-//CODE HERE
+let middleNums = numbers.splice(1,3,null)
 
 
 //////////////////PROBLEM 4////////////////////
@@ -33,4 +33,13 @@ const numbers = [1, 2, 3, 4, 5]
 
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
-// CODE HERE
+let answers = []
+
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] > 100) {
+    answers.push("big");
+  } else {
+    answers.push("small");
+  }
+}
+console.log(answers);
