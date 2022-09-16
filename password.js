4db4d8664d6841fbf213128af0ad9e34795b5845
Quinten Krauth <quintenkrauth19@gmail.com>
2022-09-16 11:50:10 -0400

    final changes

---
+++ password.js
@@ -0,0 +1,33 @@
+let password = "Karasuno19!"
+
+// asks if the password contains a letter
+if (password.includes("a"||"b"||"c"||"d"||"e"||"f"||"g"||"h"||"i"||"j"||"k"||"l"||"m"||"n"||"o"||"p"||"q"||"r"||"s"||"t"||"u"||"v"||"w"||"x"||"y"||"z")) {
+    console.log("true")
+} else {
+    console.log("false")
+}
+
+// asks if the password contains a number
+if (password.includes("1"||"2"||"3"||"4"||"5"||"6"||"7"||"8"||"9"||"0")) {
+    console.log("true")
+} else {
+    console.log("false")
+}
+
+// asks if the password contains a special character
+if (password.includes("!"||"@"||"#"||"$"||"%"||"^"||"&"||"*"||";"||":"||"."||","||"/")) {
+    console.log("true")
+} else {
+    console.log("false")
+}
+
+// makes sure password is not one of the following forbidden words
+if (password === "password") {
+    console.log("try again");
+} else if (password === "letmein") {
+    console.log("try again");
+} else {
+    console.log("password accepted");
+}
+
+// this is not perfect but its what I came up with
