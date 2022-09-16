51f311c343b4d844c5c51baf32c6b1011688dd3d
Quinten Krauth <quintenkrauth19@gmail.com>
2022-09-16 17:28:32 -0400

    final add

---
+++ strings.js
@@ -0,0 +1,60 @@
+//SOME AWESOME METHODS FOR STRINGS
+//                                         321
+let practice_with_some_strings = "Hello, world";
+//an object wrapper around all primitives String(practice_with_some_strings)
+// let str = 'hello';
+// let temp = new String("new string"); // wrapper object
+// console.log(temp);
+// temp.custom = 1;
+// console.log(temp.custom);
+
+
+//obtaining portions of a string
+//substring and return "ell" ----->
+console.log(practice_with_some_strings.substring(1,4));
+//slice and return "worl" -----> 
+console.log(practice_with_some_strings.slice(7,-2));
+//split and return an array of both words -----> 
+console.log(practice_with_some_strings.split())
+//Bonus: what method can take an array of words and put them back together
+
+//searching a string
+
+//indexOf "H" ---->
+practice_with_some_strings[0]
+
+//lastIndexOf "l" ----> 
+practice_with_some_strings[10]
+
+//boolean searching funtions
+
+//startsWith "Hello" gives us?---> 
+String.apply
+
+//endsWith "!" ---> 
+
+//includes "no" --->
+
+//creating modified version of a string
+//replace "llo" with "ya" ---> "Heya, world"
+
+//toLowerCase ---> take a guess lol
+
+//toUpperCase ---> take another guess
+
+
+
+//inspecting individual characters of a string
+//charAt 0 ---> 
+
+//charCodeAt 0  --->
+
+//Bonus: find the range of lower and uppercase letters
+
+
+//space trim funtctions !bonus mess with these methods!
+let test = "   test "
+//trim test --->
+
+//Bonus: mess with the other trims
+// //trimStart and trimEnd
